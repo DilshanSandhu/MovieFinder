6ad6d161d7fd2ad407f02da09687b51db62b565f
DIlshan <dilshan@Dilshans-MacBook-Pro.local>
2026-04-13 12:20:30 -0600

    fixed the deployement error

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import Footer from "@/components/Footer";
 import HomeClient from "@/components/HomeClient";
 import Navbar from "@/components/Navbar";
+import { Suspense } from "react";
 
 export default function HomePage() {
   return (
     <main>
       <Navbar />
-      <HomeClient />
+      <Suspense fallback={<p style={{ textAlign: "center", marginTop: "2rem" }}>Loading...</p>}>
+        <HomeClient />
+      </Suspense>
       <Footer />
     </main>
   );
